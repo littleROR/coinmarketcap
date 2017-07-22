9bb4e4f524fb6bdf08fd5e751ed3b583bdaa39ea
Bogdan <bogdan.mihai.timofte@gmail.com>
2017-07-22 19:07:05 +0300

    Fix spec

--- spec/coinmarketcap_spec.rb
+++ spec/coinmarketcap_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "pry"
 
 describe Coinmarketcap do
   it "has a version number" do
@@ -7,22 +8,18 @@ describe Coinmarketcap do
 
   describe "#coins" do
     context 'without limit' do
-      it "should receive a 200 response with all coins" do
+      it "should receive a 200 and return coins" do
         VCR.use_cassette('all_coin_response') do
-          response = Coinmarketcap.coins
-          coins = JSON.parse(response.body)
-          expect(response.code).to eq(200)
+          coins = Coinmarketcap.coins
           expect(coins.count).to be > 0
         end
       end
     end
 
     context 'with limit as 10' do
-      it "should receive a 200 response with all coins" do
+      it "should receive a 200 and return 10 coins" do
         VCR.use_cassette('limit_coin_response') do
-          response = Coinmarketcap.coins(limit = 10)
-          coins = JSON.parse(response.body)
-          expect(response.code).to eq(200)
+          coins = Coinmarketcap.coins(limit = 10)
           expect(coins.count).to eq(10)
         end
       end
@@ -33,22 +30,19 @@ describe Coinmarketcap do
     context 'with valid id' do
       it "should receive a 200 response with coin details" do
         VCR.use_cassette('single_coin_response') do
-          response = Coinmarketcap.coin('bitcoin')
-          coin = JSON.parse(response.body)
-          expect(response.code).to eq(200)
-          expect(coin.count).to eq(1)
-          expect(coin.first['id']).to eq('bitcoin')
+          coin = Coinmarketcap.coin('bitcoin')
+          expect(coin).to be_a(Coinmarketcap::Coin)
+          expect(coin.id).to eq('bitcoin')
         end
       end
     end
 
     context 'with invalid id' do
-      it "should receive a 404 response" do
+      it "should receive a 404 and return empty response" do
         VCR.use_cassette('wrong_coin_response') do
-          response = Coinmarketcap.coin('random')
-          coin = JSON.parse(response.body)
-          expect(response.code).to eq(404)
-          expect(coin['error']).to match(/id not found/)
+          coin = Coinmarketcap.coin('random')
+          expect(coin).to be_a(Coinmarketcap::Coin)
+          expect(coin.id).to be_nil
         end
       end
     end
@@ -57,20 +51,16 @@ describe Coinmarketcap do
   describe "#global" do
     it "should receive a 200 response with global details" do
       VCR.use_cassette('global_coin_response') do
-        response = Coinmarketcap.global
-        global = JSON.parse(response.body)
-        expect(response.code).to eq(200)
-        expect(global['active_currencies']).to be > 0
+        global = Coinmarketcap.global
+        expect(global.active_assets).to be > 0
       end
     end
 
     context 'with valid currency code' do
       it "should receive a 200 response with global details in that currency" do
         VCR.use_cassette('global_eur_coin_response') do
-          response = Coinmarketcap.global('EUR')
-          global = JSON.parse(response.body)
-          expect(response.code).to eq(200)
-          expect(global['total_market_cap_eur']).to be > 0
+          global = Coinmarketcap.global('EUR')
+          expect(global.total_market_cap_eur).to be > 0
         end
       end
     end
